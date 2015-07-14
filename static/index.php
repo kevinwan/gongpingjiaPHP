a0abf82c33aa02f3438d9a3d81eb76d29d2e26fc
abiao133 <276464467@qq.com>
2015-07-14 18:14:03 +0800

    1

--- static/index.php
+++ static/index.php
@@ -26,8 +26,8 @@ if ($_GET['type'] == '96x')
 {
 	$width = 90;
 	$height = 60;
-	$saveFile = APPLICATION_PATH.'/../static/cacheimg/96/'.$_GET['file'].'.png';
-	$location = 'http://static.souchela.com/cacheimg/96/'.$_GET['file'].'.png';
+	$saveFile = APPLICATION_PATH.'/../static/cacheimg/96/'.$_GET['file'];
+	$location = 'http://static.souchela.com/cacheimg/96/'.$_GET['file'];
 }
 
 //图片 120*80
@@ -35,8 +35,8 @@ if ($_GET['type'] == '128x')
 {
 	$width = 120;
 	$height = 80;
-	$saveFile = APPLICATION_PATH.'/../static/cacheimg/128/'.$_GET['file'].'.png';
-	$location = 'http://static.souchela.com/cacheimg/128/'.$_GET['file'].'.png';
+	$saveFile = APPLICATION_PATH.'/../static/cacheimg/128/'.$_GET['file'];
+	$location = 'http://static.souchela.com/cacheimg/128/'.$_GET['file'];
 }
 
 //图片 144*96
@@ -44,8 +44,8 @@ if ($_GET['type'] == '14496x')
 {
 	$width = 144;
 	$height = 96;
-	$saveFile = APPLICATION_PATH.'/../static/cacheimg/14496/'.$_GET['file'].'.png';
-	$location = 'http://static.souchela.com/cacheimg/14496/'.$_GET['file'].'.png';
+	$saveFile = APPLICATION_PATH.'/../static/cacheimg/14496/'.$_GET['file'];
+	$location = 'http://static.souchela.com/cacheimg/14496/'.$_GET['file'];
 }
 
 //图片 150*100
@@ -53,8 +53,8 @@ if ($_GET['type'] == '1510x')
 {
 	$width = 150;
 	$height = 100;
-	$saveFile = APPLICATION_PATH.'/../static/cacheimg/1510/'.$_GET['file'].'.png';
-	$location = 'http://static.souchela.com/cacheimg/1510/'.$_GET['file'].'.png';
+	$saveFile = APPLICATION_PATH.'/../static/cacheimg/1510/'.$_GET['file'];
+	$location = 'http://static.souchela.com/cacheimg/1510/'.$_GET['file'];
 }
 
 //图片 180*120  首页下方的二手车图
@@ -62,8 +62,8 @@ if ($_GET['type'] == '1812x')
 {
 	$width = 180;
 	$height = 120;
-	$saveFile = APPLICATION_PATH.'/../static/cacheimg/1812/'.$_GET['file'].'.png';
-	$location = 'http://static.souchela.com/cacheimg/1812/'.$_GET['file'].'.png';
+	$saveFile = APPLICATION_PATH.'/../static/cacheimg/1812/'.$_GET['file'];
+	$location = 'http://static.souchela.com/cacheimg/1812/'.$_GET['file'];
 }
 
 //图片 220*150  首页下方的二手车图
@@ -71,8 +71,8 @@ if ($_GET['type'] == '2215x')
 {
 	$width = 220;
 	$height = 150;
-	$saveFile = APPLICATION_PATH.'/../static/cacheimg/2215/'.$_GET['file'].'.png';
-	$location = 'http://static.souchela.com/cacheimg/2215/'.$_GET['file'].'.png';
+	$saveFile = APPLICATION_PATH.'/../static/cacheimg/2215/'.$_GET['file'];
+	$location = 'http://static.souchela.com/cacheimg/2215/'.$_GET['file'];
 }
 
 //图片 280*180  首页下方的二手车图
@@ -80,8 +80,8 @@ if ($_GET['type'] == '2818x')
 {
 	$width = 280;
 	$height = 180;
-	$saveFile = APPLICATION_PATH.'/../static/cacheimg/2818/'.$_GET['file'].'.png';
-	$location = 'http://static.souchela.com/cacheimg/2818/'.$_GET['file'].'.png';
+	$saveFile = APPLICATION_PATH.'/../static/cacheimg/2818/'.$_GET['file'];
+	$location = 'http://static.souchela.com/cacheimg/2818/'.$_GET['file'];
 }
 
 
@@ -89,8 +89,8 @@ if ($_GET['type'] == '19812x')
 {
 	$width = 198;
 	$height = 120;
-	$saveFile = APPLICATION_PATH.'/../static/cacheimg/19812/'.$_GET['file'].'.png';
-	$location = 'http://static.souchela.com/cacheimg/19812/'.$_GET['file'].'.png';
+	$saveFile = APPLICATION_PATH.'/../static/cacheimg/19812/'.$_GET['file'];
+	$location = 'http://static.souchela.com/cacheimg/19812/'.$_GET['file'];
 }
 
 
