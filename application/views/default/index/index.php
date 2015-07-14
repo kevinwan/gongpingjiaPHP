d1b4212c20b8d42d7c4134e44a0ba9ed99f0f827
abiao133 <276464467@qq.com>
2015-07-14 17:38:00 +0800

    首页

--- application/views/default/index/index.php
+++ application/views/default/index/index.php
@@ -1,21 +1,109 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title><?php echo $this->title?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="$static/css/index/base.css" rel="stylesheet" type="text/css" />
+<link href="$static/css/index/index.css" rel="stylesheet" type="text/css" />
+<script src="$static/js/jquery-1.11.1.js"></script>
+<script src="$static/js/grayscale.js"></script>
+<script src="$static/js/layer/layer.js"></script>
+<script src="$static/js/index.js"></script>
 </head>
 <body>
-<h1>Welcome to gongpingjia <?php echo $this->nowCity->name; ?>站</h1>
-<div><?php echo $this->my;?></div>
-<?php
-    foreach($this->brands as $k =>$brand)
-    {
-        echo '<br>'.$brand['name'].'<br>';
-        foreach ($brand['brandNames'] as $name)
-        {
-            echo $name.' ';
-        }
-    }
-?>
+	<div id="wrapper">
+		<div id="top">
+			<div class="wrap">
+				<div id="logo"><img src="$static/img/index/logo.png" /></div>
+				<div class="selarea">
+					<div class="label-text">当前地区：</div>
+					<div class="area">北京</div>
+				</div>
+			</div>
+		</div>
+		<div id="search">
+			<form action="" method="post">
+				<input id="switch" type="hidden" name="switch" value="sale" />
+				<div class="wrap">
+					<div class="lable-text salebtn">
+					</div>
+					<div class="label-value">
+						<div class="salebox" ></div>
+						<div class="buybox" style="display: none;"><input name="" value="2015款 大众甲壳虫 1.2TSI"></div>
+					</div>
+					<div class="subbtn"><input type="submit" class="searchbtn" value="" /></div>
+				</div>
+			</form>
+		</div>
+		<a href="#" id="carMore">随便看看...</a>
+		<div id="cdsSearchBox" style="display: none;">
+			<div class="cdsSearchMain clearfix">
+			    <div class="cdsbrandbg"></div>
+			    <div class="cds_searTabBox clearfix">
+			        <div class="cds_searTab clearfix cds_searShow">
+			            <div class="cdsSbrand clearfix">
+			                <div class="csbNav">
+			                    <a class="active" href="javascript:;">A</a><a href="javascript:;">B</a><a href="javascript:;">C</a><a href="javascript:;">D</a><a href="javascript:;">F</a><a href="javascript:;">G</a><a href="javascript:;">H</a><a href="javascript:;">J</a><a href="javascript:;">K</a><a href="javascript:;">L</a><a href="javascript:;">M</a><a href="javascript:;">N</a><a href="javascript:;">O</a><a href="javascript:;">Q</a><a href="javascript:;">R</a><a href="javascript:;">S</a><a href="javascript:;">T</a><a href="javascript:;">W</a><a href="javascript:;">X</a><a href="javascript:;">Y</a><a href="javascript:;">Z</a>
+			                </div>
+			                <div class="cdsBtabBox">
+			                	<div class="ibrandBox clearfix">
+			                		<p id="A">A</p>
+			                		<ul>
+			                			<li><img class="preview" src="$static/img/index/demo.png" /><a class="active" href="javascript:void(0);" ref="奥迪">奥迪</a></li>
+			                			<li><img class="preview" src="$static/img/index/demo.png" /><a href="javascript:void(0);" ref="奥迪">奥迪</a></li>
+			                			<li><img class="preview" src="$static/img/index/demo.png" /><a href="javascript:void(0);" ref="奥迪">奥迪</a></li>
+			                		</ul>
+			                	</div>
+			                	<div class="ibrandBox clearfix">
+			                		<p id="B">B</p>
+			                		<ul>
+			                			<li><img class="preview" src="$static/img/index/demo.png" /><a href="javascript:void(0);" ref="奥迪">奥迪</a></li>
+			                			<li><img class="preview" src="$static/img/index/demo.png" /><a href="javascript:void(0);" ref="奥迪">奥迪</a></li>
+			                			<li><img class="preview" src="$static/img/index/demo.png" /><a href="javascript:void(0);" ref="奥迪">奥迪</a></li>
+			                			<li><img class="preview" src="$static/img/index/demo.png" /><a href="javascript:void(0);" ref="奥迪">奥迪</a></li>
+			                		</ul>
+		              			</div>
+		              		</div>
+			            </div>
+			            <!--车系-->
+			       		<div class="cdsStabBox cdsStabbg" style="overflow-y: scroll; display: block;">
+			           	<!--点击品牌的车系-->
+		                    <div class="cdsStab">
+		                    	<p>东风标致</p>
+		                    	<div class="isTab clearfix">
+		                    		<a target="_blank" href="javascript:void(0);">
+		                    			<img src="http://static.cheduoshao.com/upload/autopics/2013/04/22/7b875ac6c19b0521e83f4eb4b495c0bf.jpg?128x" />
+		                    			<div><span>3008</span></div>
+		                    		</a>
+		                    		<a target="_blank" href="javascript:void(0);">
+		                    			<img src="http://static.cheduoshao.com/upload/autopics/2014/04/09/338b6089a350bbadb1aa1a8767ecd416.jpg?128x" />
+		                    			<div><span>301</span></div>
+		                    		</a>
+		                    		<a target="_blank" href="javascript:void(0);">
+		                    			<img src="http://static.cheduoshao.com/upload/autopics/2014/04/09/14ff672cc69fe37908c45d64b60014f6.jpg?128x" />
+		                    			<div><span>308</span></div>
+		                    		</a>
+		                    		<a target="_blank" href="javascript:void(0);">
+		                    			<img src="http://static.cheduoshao.com/upload/autopics/2014/04/09/6a7d2df06cc322bd58b9c2ae432164c0.jpg?128x" />
+		                    			<div><span>408</span></div>
+		                    		</a>
+		                    		<a target="_blank" href="javascript:void(0);">
+		                    			<img src="http://static.cheduoshao.com/upload/autopics/2014/04/09/7aca0d44b39a8aa2dcb8a1c092ecb785.jpg?128x" />
+		                    			<div><span>508</span></div>
+		                    		</a>
+		                    		<a target="_blank" href="javascript:void(0);">
+		                    			<img src="http://static.cheduoshao.com/upload/autopics/2014/05/20/f1b6ac44f170e1c88a470867383b0b8c.jpg?128x" />
+		                    			<div><span>2008</span></div>
+		                    		</a>
+		                    	</div>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+		    </div>
+		</div>
+	</div>
+	<div id="temp_div">
+		<img class="shade" src="" />
+	</div>
 </body>
 </html>
