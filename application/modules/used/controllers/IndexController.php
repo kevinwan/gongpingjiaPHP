fdc798d0c36a0071459988e9dccd971c8d006645
abiao133 <276464467@qq.com>
2015-07-16 16:17:25 +0800

    used

---
+++ application/modules/used/controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 估值报告控制器
+ * 
+ * @author abiao 2015-7-16
+ */
+class Used_IndexController extends XF_Controller_Abstract
+{
+    public function __construct()
+    {
+        parent::__construct($this);
+    }
+    
+    public function indexAction()
+    {
+        
+    }
+}
+
