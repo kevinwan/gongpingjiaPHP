576b0d51395089f61f83684f6e25fabf1fa0b2ac
abiao133 <276464467@qq.com>
2015-07-22 17:39:19 +0800

    -jiekou

--- application/layouts/scripts/selectcity.php
+++ application/layouts/scripts/selectcity.php
@@ -1,11 +1,11 @@
 <div id="selarea" style="display: none;">
-	<div class="dialogs-title">猜你要去 <a class="defsel" href="#">北京站</a> 或者 <a href="#">上海</a> <a href="#">广东</a> <a href="#">重庆</a> <a href="#">江苏</a> <a href="#">山西</a> <a href="#">山东</a></div>
+	<div class="dialogs-title">猜你要去 <a class="defsel" href="http://beijing.<?php echo $domain?>">北京站</a> 或者 <a href="http://shanghai.<?php echo $domain?>">上海</a> <a href="http://guangdong.<?php echo $domain?>">广东</a> <a href="http://chongqing.<?php echo $domain?>">重庆</a> <a href="http://jiangsu.<?php echo $domain?>">江苏</a> <a href="http://shanxi.<?php echo $domain?>">山西</a> <a href="http://shandong.<?php echo $domain?>">山东</a></div>
 	<div class="dialogs-cont">
 		<p class="subtitle">请选择您的地区：</p>
 		<div class="area clearfix">
 			<div class="label-text">直辖市</div>
 			<div class="label-value">
-				<a class="defsel" href="#">北京</a> <a href="#">上海</a> <a href="#">重庆</a> <a href="#">天津</a>
+				<a class="defsel" href="http://beijing.<?php echo $domain?>">北京</a> <a href="http://shanghai.<?php echo $domain?>">上海</a> <a href="http://chongqing.<?php echo $domain?>">重庆</a> <a href="http://tianjin.<?php echo $domain?>">天津</a>
 			</div>
 		</div>
 		<div class="area clearfix">
