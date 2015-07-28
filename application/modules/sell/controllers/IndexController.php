51a9fe2d9997d70962fc757a51da3e24af686f1c
jianrock <y4happy@2980.com>
2015-07-28 11:17:01 +0800

    修改兼容性

--- application/modules/sell/controllers/IndexController.php
+++ application/modules/sell/controllers/IndexController.php
@@ -154,7 +154,8 @@ class Sell_IndexController extends XF_Controller_Abstract
 
         // usleep(5000);
 
-        $targetDir = "/wwwroot/gongpingjiaPHP/static/uploads/car/";
+//        $targetDir = "D:/xampp/htdocs/pahaoche/gongpingjia/static/uploads/car/".gmdate("YmdHis")."/";
+        $targetDir = "/wwwroot/gongpingjiaPHP/static/uploads/car/".gmdate("YMdHis")."/";
 
         // $targetDir = 'uploads';
         $cleanupTargetDir = true;
