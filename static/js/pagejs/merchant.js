89eab2ee18e5fc4295169a7f0b03451734fc66df
jianrock <y4happy@2980.com>
2015-07-31 14:39:33 +0800

    添加拍卖平台到商家选择列表中。

--- static/js/pagejs/merchant.js
+++ static/js/pagejs/merchant.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$(document).on("click", ".saler_main .saler_choose .saler_choose_part", function() {
 		var theobj = $(this).attr("id");
 		var thiary = theobj.split("_");
-		window.location.href='/sell/index/personinfo?infoId='+thiary["1"]+"&comName="+$(this).attr("title");
+		window.location.href='/sell/index/personinfo?dataFrom='+thiary["0"]+'&infoId='+thiary["1"]+"&comName="+$(this).attr("title");
 	});
     $(document).on("click", ".saler_main .saler_other", function() {
         $(".saler_main .saler_choose .otherDealers").slideDown();
